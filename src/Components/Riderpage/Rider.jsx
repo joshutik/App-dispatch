b54fc3390ce177dfa06307c8e24bb4d254bba147
joshutik <utik2.4real@gmail.com>
2023-11-13 22:40:15 +0100

    added some pages

--- src/Components/Riderpage/Rider.jsx
+++ src/Components/Riderpage/Rider.jsx
@@ -12,29 +12,25 @@ const Rider = () => {
           <p className="fs-3 text-light">Buenas noches,</p>
           <h1 className="text-light text-center fw-bold">ADMIN DE LOTERIA</h1>
         </div>
-
+    <div className="row">
+      <div className="col-lg-12">
         <div className="card">
-          <div className="card-body">
+          <div className="card-body w-75">
             <div>
               <input type="search" className="form-control rounded-pill mt-5" />
-              <div className="row">
+              <div className="row mt-5">
                 <div className="col-lg-4">
-                  <div className="card ">
-                    <img src="" alt="" />
-                  </div>
-                </div>
-                <div className="col-lg-4">
-                  <div className="card">
+                  <div className="card rounded-5">
                     <img src={img1} alt="" />
                   </div>
                 </div>
                 <div className="col-lg-4">
-                  <div className="card">
+                  <div className="card rounded-5">
                     <img src={img2} alt="" />
                   </div>
                 </div>
                 <div className="col-lg-4">
-                  <div className="card">
+                  <div className="card rounded-5">
                     <img src={img3} alt="" />
                   </div>
                 </div>
@@ -43,6 +39,8 @@ const Rider = () => {
           </div>
         </div>
       </div>
+    </div>
+    </div>
     </div>
   );
 };
