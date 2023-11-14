2b6c01572f5f8d103efc548ff5ed79755d03b804
joshutik <utik2.4real@gmail.com>
2023-11-14 12:20:01 +0100

    made changes to the page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Landingpage from './Components/Landpage/Landingpage';
-import Lotery from './Components/Lottry/Lotery';
+// import Lotery from './Components/Lottry/Lotery';
 import Ownerslogin from './Components/Ownerlogin/Ownerslogin';
 import Adminslogin from './Components/Adminlogin/Adminslogin';
-import Confirm from './Components/Confirmation/Confirm';
-import Rider from './Components/Riderpage/Rider';
-import Ownerslog from './Components/Overview/Ownerslog';
-import Lottery from './Components/Lotery/Lottery';
+// import Confirm from './Components/Confirmation/Confirm';
+// import Rider from './Components/Riderpage/Rider';
+// import Ownerslog from './Components/Overview/Ownerslog';
+// import Lottery from './Components/Lotery/Lottery';
 
 
 function App() {
