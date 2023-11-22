0a9b9bcea5a7fced494c042ee53d4bb2edcbdeed
joshutik <utik2.4real@gmail.com>
2023-11-22 12:26:04 +0100

    fixed the form input borders

--- src/Components/Managerpage/Managerpage.jsx
+++ src/Components/Managerpage/Managerpage.jsx
@@ -44,15 +44,15 @@ const Managerpage = () => {
               placeholder="Search orders...."
             />
           </div>
-          <div className="row justify-content-between w-75 mb-3">
+          <div className="row justify-content-between w-75 mb-3 gy-3">
             <div className="fs-5 icon col-lg-12 col-md-6 col-sm-12">
-              <Link to="/add-order" className="icon">
+              <Link to="/add-order" className="icon text-decoration-none">
                 {" "}
                 <i class="bi bi-plus-lg"></i>Add new order
               </Link>
             </div>
             <div className="fs-5 col-lg-12 col-md-6 col-sm-12">
-              <Link to="/edit-company" className="icon">
+              <Link to="/edit-company" className="icon text-decoration-none">
                 <i class="bi bi-plus-lg fw-bolder fs-4"></i>Edit Company details
               </Link>
             </div>
